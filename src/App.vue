46e200def0712d0ba1cbbd553c7ed8accc232099
liheng <1061555401@qq.com>
2021-02-07 23:35:20 +0800

    图表列表样式

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,10 @@
 <template>
 	<div class="page">
-		<!-- <div>
-			<div class="layoutJSON">
-				Displayed as <code>[x, y, w, h]</code>:
-				<div class="columns">
-					<div class="layoutItem" v-for="item in layout" :key="item.i">
-						<b>{{ item.i }}</b
-						>: [{{ item.x }}, {{ item.y }}, {{ item.w }}, {{ item.h }}]
-					</div>
-				</div>
-			</div>
-		</div>
-		<br /> -->
-
 		<grid-layout
 			ref="gridlayout"
 			class="drappable-content"
 			:layout.sync="layout"
-			:col-num="12"
+			:col-num="6"
 			:row-height="30"
 			:is-draggable="true"
 			:is-resizable="true"
@@ -34,26 +21,22 @@
 				:h="item.h"
 				:i="item.i"
 			>
-				<span class="text">{{ item.i }}</span>
+				<!-- <span class="text">{{ item.i }}</span> -->
 			</grid-item>
 		</grid-layout>
-		<div>
-			<a-tabs default-active-key="1" class="droppable-list">
-				<a-tab-pane tab="2 X 2">
-					<v-chart
-						class="chart"
-						v-for="(chart, index) in allCharts.tab1"
-						:key="index"
-						:ref="'chartA' + index"
-						:options="chart"
-						@drag.native="drag"
-						@dragend.native="dragend($event, chart)"
-						draggable="true"
-						unselectable="on"
-					/>
-				</a-tab-pane>
-			</a-tabs>
-		</div>
+		<a-tabs :default-active-key="2" class="droppable-list" :tabBarStyle="{ 'margin-bottom': 0 }">
+			<a-tab-pane :tab="'2 X' + tabIndex" v-for="tabIndex in 3" :key="tabIndex" class="tabPane">
+				<v-chart
+					v-for="(chart, index) in allCharts['tab' + tabIndex]"
+					:key="index"
+					:options="chart.echartOption"
+					@drag.native="drag(chart.gridOption)"
+					@dragend.native="dragend(chart.echartOption, chart.gridOption)"
+					draggable="true"
+					unselectable="on"
+				/>
+			</a-tab-pane>
+		</a-tabs>
 	</div>
 </template>
 
@@ -74,184 +57,6 @@
 		data() {
 			return {
 				layout: [],
-				echartOption: {
-					xAxis: {
-						type: "category",
-						data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-					},
-					yAxis: {
-						type: "value",
-					},
-					series: [
-						{
-							data: [150, 230, 224, 218, 135, 147, 260],
-							type: "line",
-						},
-					],
-				},
-				echartOption2: {
-					color: ["#80FFA5", "#00DDFF", "#37A2FF", "#FF0087", "#FFBF00"],
-					grid: {
-						left: "3%",
-						right: "4%",
-						bottom: "3%",
-						containLabel: true,
-					},
-					xAxis: [
-						{
-							type: "category",
-							boundaryGap: false,
-							data: ["周一", "周二", "周三", "周四", "周五", "周六", "周日"],
-						},
-					],
-					yAxis: [
-						{
-							type: "value",
-						},
-					],
-					series: [
-						{
-							name: "Line 1",
-							type: "line",
-							stack: "总量",
-							smooth: true,
-							lineStyle: {
-								width: 0,
-							},
-							showSymbol: false,
-							areaStyle: {
-								opacity: 0.8,
-								color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-									{
-										offset: 0,
-										color: "rgba(128, 255, 165)",
-									},
-									{
-										offset: 1,
-										color: "rgba(1, 191, 236)",
-									},
-								]),
-							},
-							emphasis: {
-								focus: "series",
-							},
-							data: [140, 232, 101, 264, 90, 340, 250],
-						},
-						{
-							name: "Line 2",
-							type: "line",
-							stack: "总量",
-							smooth: true,
-							lineStyle: {
-								width: 0,
-							},
-							showSymbol: false,
-							areaStyle: {
-								opacity: 0.8,
-								color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-									{
-										offset: 0,
-										color: "rgba(0, 221, 255)",
-									},
-									{
-										offset: 1,
-										color: "rgba(77, 119, 255)",
-									},
-								]),
-							},
-							emphasis: {
-								focus: "series",
-							},
-							data: [120, 282, 111, 234, 220, 340, 310],
-						},
-						{
-							name: "Line 3",
-							type: "line",
-							stack: "总量",
-							smooth: true,
-							lineStyle: {
-								width: 0,
-							},
-							showSymbol: false,
-							areaStyle: {
-								opacity: 0.8,
-								color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-									{
-										offset: 0,
-										color: "rgba(55, 162, 255)",
-									},
-									{
-										offset: 1,
-										color: "rgba(116, 21, 219)",
-									},
-								]),
-							},
-							emphasis: {
-								focus: "series",
-							},
-							data: [320, 132, 201, 334, 190, 130, 220],
-						},
-						{
-							name: "Line 4",
-							type: "line",
-							stack: "总量",
-							smooth: true,
-							lineStyle: {
-								width: 0,
-							},
-							showSymbol: false,
-							areaStyle: {
-								opacity: 0.8,
-								color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-									{
-										offset: 0,
-										color: "rgba(255, 0, 135)",
-									},
-									{
-										offset: 1,
-										color: "rgba(135, 0, 157)",
-									},
-								]),
-							},
-							emphasis: {
-								focus: "series",
-							},
-							data: [220, 402, 231, 134, 190, 230, 120],
-						},
-						{
-							name: "Line 5",
-							type: "line",
-							stack: "总量",
-							smooth: true,
-							lineStyle: {
-								width: 0,
-							},
-							showSymbol: false,
-							label: {
-								show: true,
-								position: "top",
-							},
-							areaStyle: {
-								opacity: 0.8,
-								color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-									{
-										offset: 0,
-										color: "rgba(255, 191, 0)",
-									},
-									{
-										offset: 1,
-										color: "rgba(224, 62, 76)",
-									},
-								]),
-							},
-							emphasis: {
-								focus: "series",
-							},
-							data: [220, 302, 181, 234, 210, 290, 150],
-						},
-					],
-				},
-				once: true,
 			};
 		},
 		computed: {
@@ -268,7 +73,7 @@
 			);
 		},
 		methods: {
-			drag: function(e) {
+			drag: function(gridOption) {
 				let parentRect = this.$refs.gridlayout.$el.getBoundingClientRect();
 				let mouseInGrid = false;
 				if (
@@ -281,37 +86,52 @@
 				}
 				if (mouseInGrid === true && this.layout.findIndex((item) => item.i === "drop") === -1) {
 					this.layout.push({
-						x: (this.layout.length * 2) % (this.colNum || 12),
-						y: this.layout.length + (this.colNum || 12), // puts it at the bottom
-						w: 2,
-						h: 2,
+						x: this.layout.length % (this.colNum || 12),
+						y: Math.floor(this.layout.length / (this.colNum || 12)), // puts it at the bottom
+						w: gridOption.w,
+						h: gridOption.h,
 						i: "drop",
 					});
 				}
 				let index = this.layout.findIndex((item) => item.i === "drop");
 				if (index !== -1) {
 					try {
-						this.$refs.gridlayout.$children[this.layout.length].$refs.item.style.display = "none";
+						// this.$refs.gridlayout.$children[this.layout.length].$refs.item.style.display = "none";
 					} catch {}
 					let el = this.$refs.gridlayout.$children[index];
 					el.dragging = {
+						//TODO: 可删除？
 						top: mouseXY.y - parentRect.top,
 						left: mouseXY.x - parentRect.left,
 					};
 					let new_pos = el.calcXY(mouseXY.y - parentRect.top, mouseXY.x - parentRect.left);
 					if (mouseInGrid === true) {
-						this.$refs.gridlayout.dragEvent("dragstart", "drop", new_pos.x, new_pos.y, 1, 1);
+						this.$refs.gridlayout.dragEvent(
+							"dragstart",
+							"drop",
+							new_pos.x,
+							new_pos.y,
+							gridOption.h,
+							gridOption.w
+						);
 						DragPos.i = String(index);
 						DragPos.x = this.layout[index].x;
 						DragPos.y = this.layout[index].y;
 					}
 					if (mouseInGrid === false) {
-						this.$refs.gridlayout.dragEvent("dragend", "drop", new_pos.x, new_pos.y, 1, 1);
+						this.$refs.gridlayout.dragEvent(
+							"dragend",
+							"drop",
+							new_pos.x,
+							new_pos.y,
+							gridOption.h,
+							gridOption.w
+						);
 						this.layout = this.layout.filter((obj) => obj.i !== "drop");
 					}
 				}
 			},
-			dragend: function(e, thisChart) {
+			dragend: function(thisChart, gridOption) {
 				let parentRect = this.$refs.gridlayout.$el.getBoundingClientRect();
 				let mouseInGrid = false;
 				if (
@@ -328,20 +148,18 @@
 					this.layout.push({
 						x: DragPos.x,
 						y: DragPos.y,
-						w: 2,
-						h: 2,
+						w: gridOption.w,
+						h: gridOption.h,
 						i: DragPos.i,
 					});
 					this.$refs.gridlayout.dragEvent("dragend", DragPos.i, DragPos.x, DragPos.y, 1, 1);
-					// try {
 					this.$nextTick(() => {
-						this.$refs.gridlayout.$children[this.layout.length].$refs.item.style.display = "block";
-						console.log(this.$refs.gridlayout.$children[this.layout.length].$refs.item);
+						// this.$refs.gridlayout.$children[this.layout.length].$refs.item.style.display = "block";
+						// console.log(this.$refs.gridlayout.$children[this.layout.length].$refs.item);
 						echarts
 							.init(this.$refs.gridlayout.$children[this.layout.length].$refs.item)
 							.setOption(thisChart);
 					});
-					// } catch {}
 				}
 			},
 		},
@@ -349,84 +167,31 @@
 </script>
 
 <style scoped>
-	.droppable-element {
-		width: 50vw;
-		text-align: center;
-		padding: 10px;
-	}
 	.vue-grid-layout {
 		background: #eee;
 	}
-	.vue-grid-item:not(.vue-grid-placeholder) {
-		/* background: #ccc; */
-		border: 1px solid black;
-	}
-	.vue-grid-item .resizing {
-		opacity: 0.9;
-	}
-	.vue-grid-item .static {
-		/* background: #cce; */
-	}
-	.vue-grid-item .text {
-		font-size: 24px;
-		text-align: center;
-		position: absolute;
-		top: 0;
-		bottom: 0;
-		left: 0;
-		right: 0;
-		margin: auto;
-		height: 100%;
-		width: 100%;
-	}
-	.vue-grid-item .no-drag {
-		height: 100%;
-		width: 100%;
-	}
-	.vue-grid-item .minMax {
-		font-size: 12px;
-	}
-	.vue-grid-item .add {
-		cursor: pointer;
-	}
-	.vue-draggable-handle {
-		position: absolute;
-		width: 20px;
-		height: 20px;
-		top: 0;
-		left: 0;
-		background: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='10' height='10'><circle cx='5' cy='5' r='5' fill='#999999'/></svg>")
-			no-repeat;
-		background-position: bottom right;
-		padding: 0 8px 8px 0;
-		background-repeat: no-repeat;
-		background-origin: content-box;
-		box-sizing: border-box;
-		cursor: pointer;
-	}
-	.layoutJSON {
-		background: #ddd;
-		border: 1px solid black;
-		margin-top: 10px;
-		padding: 10px;
-	}
-	.layoutJSON {
-		background: #ddd;
-		border: 1px solid black;
-		margin-top: 10px;
-		padding: 10px;
-	}
-	.columns {
-		-moz-columns: 120px;
-		-webkit-columns: 120px;
-		columns: 120px;
-	}
-
 	.page {
 		display: flex;
 	}
+	.droppable-list {
+		flex: 1;
+		text-align: center;
+		height: 100vh;
+	}
 	.drappable-content {
-		width: 100%;
+		flex: 2;
 		height: 100vh !important;
+	}
+	.tabPane {
+		display: flex;
+		height: 100%;
+		width: 100%;
+		padding: 0 20px 50px;
+		flex-direction: column;
+	}
+	/*v-echart标签的类*/
+	.echarts {
+		/* 修改vue-echarts的默认大小 */
+		width: 100% !important;
 	}
 </style>
--- src/echarts.js
+++ src/echarts.js
@@ -1,359 +1,409 @@
+// echarts和vue-grid-layout的配置对象举例
 export default {
   tab1: [
     {
-      xAxis: {
-        type: 'category',
-        boundaryGap: false,
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      gridOption: {
+        w: 2,
+        h: 1,
+        i: 11,
       },
-      yAxis: {
-        type: 'value'
+      echartOption: {
+        grid: {
+          left: '0%',
+          right: '0%',
+          bottom: '0%',
+          containLabel: true
+        },
+        xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [{
+          data: [820, 932, 901, 934, 1290, 1330, 1320],
+          type: 'line',
+          areaStyle: {}
+        }]
       },
-      series: [{
-        data: [820, 932, 901, 934, 1290, 1330, 1320],
-        type: 'line',
-        areaStyle: {}
-      }]
     }, {
-      xAxis: {
-        type: 'category',
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      gridOption: {
+        w: 2,
+        h: 1,
+        i: 12,
       },
-      yAxis: {
-        type: 'value'
-      },
-      series: [{
-        data: [150, 230, 224, 218, 135, 147, 260],
-        type: 'line'
-      }]
+      echartOption: {
+        grid: {
+          left: '0%',
+          right: '0%',
+          bottom: '0%',
+          containLabel: true
+        },
+        xAxis: {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [{
+          data: [150, 230, 224, 218, 135, 147, 260],
+          type: 'line'
+        }]
+      }
     },
     {
-      title: {
-        text: '对数轴示例',
-        left: 'center'
+      gridOption: {
+        w: 2,
+        h: 1,
+        i: 13,
       },
-      tooltip: {
-        trigger: 'item',
-        formatter: '{a} <br/>{b} : {c}'
-      },
-      legend: {
-        left: 'left',
-        data: ['2的指数', '3的指数']
-      },
-      xAxis: {
-        type: 'category',
-        name: 'x',
-        splitLine: { show: false },
-        data: ['一', '二', '三', '四', '五', '六', '七', '八', '九']
-      },
-      grid: {
-        left: '3%',
-        right: '4%',
-        bottom: '3%',
-        containLabel: true
-      },
-      yAxis: {
-        type: 'log',
-        name: 'y',
-        minorSplitLine: {
-          show: true
-        }
-      },
-      series: [
-        {
-          name: '3的指数',
-          type: 'line',
-          data: [1, 3, 9, 27, 81, 247, 741, 2223, 6669]
+      echartOption: {
+        xAxis: {
+          type: 'category',
+          name: 'x',
+          splitLine: { show: false },
+          data: ['一', '二', '三', '四', '五', '六', '七', '八', '九']
         },
-        {
-          name: '2的指数',
-          type: 'line',
-          data: [1, 2, 4, 8, 16, 32, 64, 128, 256]
+        grid: {
+          left: '0%',
+          right: '0%',
+          bottom: '0%',
+          containLabel: true
         },
-        {
-          name: '1/2的指数',
-          type: 'line',
-          data: [1 / 2, 1 / 4, 1 / 8, 1 / 16, 1 / 32, 1 / 64, 1 / 128, 1 / 256, 1 / 512]
-        }
-      ]
+        yAxis: {
+          type: 'log',
+          name: 'y',
+          minorSplitLine: {
+            show: true
+          }
+        },
+        series: [
+          {
+            name: '3的指数',
+            type: 'line',
+            data: [1, 3, 9, 27, 81, 247, 741, 2223, 6669]
+          },
+          {
+            name: '2的指数',
+            type: 'line',
+            data: [1, 2, 4, 8, 16, 32, 64, 128, 256]
+          },
+          {
+            name: '1/2的指数',
+            type: 'line',
+            data: [1 / 2, 1 / 4, 1 / 8, 1 / 16, 1 / 32, 1 / 64, 1 / 128, 1 / 256, 1 / 512]
+          }
+        ]
+      }
     },
     {
-      title: {
-        text: '世界人口总量',
-        subtext: '数据来自网络'
+      gridOption: {
+        w: 2,
+        h: 1,
+        i: 14,
       },
-      tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-          type: 'shadow'
-        }
-      },
-      legend: {
-        data: ['2011年', '2012年']
-      },
-      grid: {
-        left: '3%',
-        right: '4%',
-        bottom: '3%',
-        containLabel: true
-      },
-      xAxis: {
-        type: 'value',
-        boundaryGap: [0, 0.01]
-      },
-      yAxis: {
-        type: 'category',
-        data: ['巴西', '印尼', '美国', '印度', '中国', '世界人口(万)']
-      },
-      series: [
-        {
-          name: '2011年',
-          type: 'bar',
-          data: [18203, 23489, 29034, 104970, 131744, 630230]
+      echartOption: {
+        grid: {
+          left: '-14%',
+          right: '0%',
+          bottom: '0%',
+          containLabel: true
         },
-        {
-          name: '2012年',
-          type: 'bar',
-          data: [19325, 23438, 31000, 121594, 134141, 681807]
-        }
-      ]
+        xAxis: {
+          type: 'value',
+          boundaryGap: [0, 0.01]
+        },
+        yAxis: {
+          type: 'category',
+          data: ['巴西', '印尼', '美国', '印度', '中国', '世界人口(万)']
+        },
+        series: [
+          {
+            name: '2011年',
+            type: 'bar',
+            data: [18203, 23489, 29034, 104970, 131744, 630230]
+          },
+          {
+            name: '2012年',
+            type: 'bar',
+            data: [19325, 23438, 31000, 121594, 134141, 681807]
+          }
+        ]
+      }
     }],
   tab2: [
     {
-      xAxis: {
-        type: 'category',
-        boundaryGap: false,
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      gridOption: {
+        w: 2,
+        h: 2,
+        i: 21,
       },
-      yAxis: {
-        type: 'value'
+      echartOption: {
+        grid: {
+          left: '0%',
+          right: '0%',
+          bottom: '0%',
+          containLabel: true
+        },
+        xAxis: {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [{
+          data: [150, 230, 224, 218, 135, 147, 260],
+          type: 'line'
+        }]
+      }
+    },
+    {
+      gridOption: {
+        w: 2,
+        h: 2,
+        i: 22,
       },
-      series: [{
-        data: [820, 932, 901, 934, 1290, 1330, 1320],
-        type: 'line',
-        areaStyle: {}
-      }]
+      echartOption: {
+        grid: {
+          left: '0%',
+          right: '0%',
+          bottom: '0%',
+          containLabel: true
+        },
+        xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [{
+          data: [820, 932, 901, 934, 1290, 1330, 1320],
+          type: 'line',
+          areaStyle: {}
+        }]
+      }
+    },
+    {
+      gridOption: {
+        w: 2,
+        h: 2,
+        i: 23,
+      },
+      echartOption: {
+
+
+        grid: {
+          left: '-14%',
+          right: '0%',
+          bottom: '0%',
+          containLabel: true
+        },
+        xAxis: {
+          type: 'value',
+          boundaryGap: [0, 0.01]
+        },
+        yAxis: {
+          type: 'category',
+          data: ['巴西', '印尼', '美国', '印度', '中国', '世界人口(万)']
+        },
+        series: [
+          {
+            name: '2011年',
+            type: 'bar',
+            data: [18203, 23489, 29034, 104970, 131744, 630230]
+          },
+          {
+            name: '2012年',
+            type: 'bar',
+            data: [19325, 23438, 31000, 121594, 134141, 681807]
+          }
+        ]
+      }
     }, {
-      xAxis: {
-        type: 'category',
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      gridOption: {
+        w: 2,
+        h: 2,
+        i: 24,
       },
-      yAxis: {
-        type: 'value'
-      },
-      series: [{
-        data: [150, 230, 224, 218, 135, 147, 260],
-        type: 'line'
-      }]
-    },
-    {
-      title: {
-        text: '对数轴示例',
-        left: 'center'
-      },
-      tooltip: {
-        trigger: 'item',
-        formatter: '{a} <br/>{b} : {c}'
-      },
-      legend: {
-        left: 'left',
-        data: ['2的指数', '3的指数']
-      },
-      xAxis: {
-        type: 'category',
-        name: 'x',
-        splitLine: { show: false },
-        data: ['一', '二', '三', '四', '五', '六', '七', '八', '九']
-      },
-      grid: {
-        left: '3%',
-        right: '4%',
-        bottom: '3%',
-        containLabel: true
-      },
-      yAxis: {
-        type: 'log',
-        name: 'y',
-        minorSplitLine: {
-          show: true
-        }
-      },
-      series: [
-        {
-          name: '3的指数',
-          type: 'line',
-          data: [1, 3, 9, 27, 81, 247, 741, 2223, 6669]
+      echartOption: {
+
+        xAxis: {
+          type: 'category',
+          name: 'x',
+          splitLine: { show: false },
+          data: ['一', '二', '三', '四', '五', '六', '七', '八', '九']
         },
-        {
-          name: '2的指数',
-          type: 'line',
-          data: [1, 2, 4, 8, 16, 32, 64, 128, 256]
+        grid: {
+          left: '0%',
+          right: '0%',
+          bottom: '0%',
+          containLabel: true
         },
-        {
-          name: '1/2的指数',
-          type: 'line',
-          data: [1 / 2, 1 / 4, 1 / 8, 1 / 16, 1 / 32, 1 / 64, 1 / 128, 1 / 256, 1 / 512]
-        }
-      ]
-    },
-    {
-      title: {
-        text: '世界人口总量',
-        subtext: '数据来自网络'
-      },
-      tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-          type: 'shadow'
-        }
-      },
-      legend: {
-        data: ['2011年', '2012年']
-      },
-      grid: {
-        left: '3%',
-        right: '4%',
-        bottom: '3%',
-        containLabel: true
-      },
-      xAxis: {
-        type: 'value',
-        boundaryGap: [0, 0.01]
-      },
-      yAxis: {
-        type: 'category',
-        data: ['巴西', '印尼', '美国', '印度', '中国', '世界人口(万)']
-      },
-      series: [
-        {
-          name: '2011年',
-          type: 'bar',
-          data: [18203, 23489, 29034, 104970, 131744, 630230]
+        yAxis: {
+          type: 'log',
+          name: 'y',
+          minorSplitLine: {
+            show: true
+          }
         },
-        {
-          name: '2012年',
-          type: 'bar',
-          data: [19325, 23438, 31000, 121594, 134141, 681807]
-        }
-      ]
-    }],
+        series: [
+          {
+            name: '3的指数',
+            type: 'line',
+            data: [1, 3, 9, 27, 81, 247, 741, 2223, 6669]
+          },
+          {
+            name: '2的指数',
+            type: 'line',
+            data: [1, 2, 4, 8, 16, 32, 64, 128, 256]
+          },
+          {
+            name: '1/2的指数',
+            type: 'line',
+            data: [1 / 2, 1 / 4, 1 / 8, 1 / 16, 1 / 32, 1 / 64, 1 / 128, 1 / 256, 1 / 512]
+          }
+        ]
+      }
+    },],
   tab3: [
     {
-      xAxis: {
-        type: 'category',
-        boundaryGap: false,
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      gridOption: {
+        w: 2,
+        h: 3,
+        i: 31,
       },
-      yAxis: {
-        type: 'value'
+      echartOption: {
+        grid: {
+          left: '-14%',
+          right: '0%',
+          bottom: '0%',
+          containLabel: true
+        },
+        xAxis: {
+          type: 'value',
+          boundaryGap: [0, 0.01]
+        },
+        yAxis: {
+          type: 'category',
+          data: ['巴西', '印尼', '美国', '印度', '中国', '世界人口(万)']
+        },
+        series: [
+          {
+            name: '2011年',
+            type: 'bar',
+            data: [18203, 23489, 29034, 104970, 131744, 630230]
+          },
+          {
+            name: '2012年',
+            type: 'bar',
+            data: [19325, 23438, 31000, 121594, 134141, 681807]
+          }
+        ]
+      }
+    },
+    {
+      gridOption: {
+        w: 2,
+        h: 3,
+        i: 32,
       },
-      series: [{
-        data: [820, 932, 901, 934, 1290, 1330, 1320],
-        type: 'line',
-        areaStyle: {}
-      }]
+      echartOption: {
+        grid: {
+          left: '0%',
+          right: '0%',
+          bottom: '0%',
+          containLabel: true
+        },
+        xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [{
+          data: [820, 932, 901, 934, 1290, 1330, 1320],
+          type: 'line',
+          areaStyle: {}
+        }]
+      }
     }, {
-      xAxis: {
-        type: 'category',
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      gridOption: {
+        w: 2,
+        h: 3,
+        i: 33,
       },
-      yAxis: {
-        type: 'value'
-      },
-      series: [{
-        data: [150, 230, 224, 218, 135, 147, 260],
-        type: 'line'
-      }]
-    },
-    {
-      title: {
-        text: '对数轴示例',
-        left: 'center'
-      },
-      tooltip: {
-        trigger: 'item',
-        formatter: '{a} <br/>{b} : {c}'
-      },
-      legend: {
-        left: 'left',
-        data: ['2的指数', '3的指数']
-      },
-      xAxis: {
-        type: 'category',
-        name: 'x',
-        splitLine: { show: false },
-        data: ['一', '二', '三', '四', '五', '六', '七', '八', '九']
-      },
-      grid: {
-        left: '3%',
-        right: '4%',
-        bottom: '3%',
-        containLabel: true
-      },
-      yAxis: {
-        type: 'log',
-        name: 'y',
-        minorSplitLine: {
-          show: true
-        }
-      },
-      series: [
-        {
-          name: '3的指数',
-          type: 'line',
-          data: [1, 3, 9, 27, 81, 247, 741, 2223, 6669]
+      echartOption: {
+
+        xAxis: {
+          type: 'category',
+          name: 'x',
+          splitLine: { show: false },
+          data: ['一', '二', '三', '四', '五', '六', '七', '八', '九']
         },
-        {
-          name: '2的指数',
-          type: 'line',
-          data: [1, 2, 4, 8, 16, 32, 64, 128, 256]
+        grid: {
+          left: '0%',
+          right: '0%',
+          bottom: '0%',
+          containLabel: true
         },
-        {
-          name: '1/2的指数',
-          type: 'line',
-          data: [1 / 2, 1 / 4, 1 / 8, 1 / 16, 1 / 32, 1 / 64, 1 / 128, 1 / 256, 1 / 512]
-        }
-      ]
-    },
-    {
-      title: {
-        text: '世界人口总量',
-        subtext: '数据来自网络'
-      },
-      tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-          type: 'shadow'
-        }
-      },
-      legend: {
-        data: ['2011年', '2012年']
-      },
-      grid: {
-        left: '3%',
-        right: '4%',
-        bottom: '3%',
-        containLabel: true
-      },
-      xAxis: {
-        type: 'value',
-        boundaryGap: [0, 0.01]
-      },
-      yAxis: {
-        type: 'category',
-        data: ['巴西', '印尼', '美国', '印度', '中国', '世界人口(万)']
-      },
-      series: [
-        {
-          name: '2011年',
-          type: 'bar',
-          data: [18203, 23489, 29034, 104970, 131744, 630230]
+        yAxis: {
+          type: 'log',
+          name: 'y',
+          minorSplitLine: {
+            show: true
+          }
         },
-        {
-          name: '2012年',
-          type: 'bar',
-          data: [19325, 23438, 31000, 121594, 134141, 681807]
-        }
-      ]
-    }]
+        series: [
+          {
+            name: '3的指数',
+            type: 'line',
+            data: [1, 3, 9, 27, 81, 247, 741, 2223, 6669]
+          },
+          {
+            name: '2的指数',
+            type: 'line',
+            data: [1, 2, 4, 8, 16, 32, 64, 128, 256]
+          },
+          {
+            name: '1/2的指数',
+            type: 'line',
+            data: [1 / 2, 1 / 4, 1 / 8, 1 / 16, 1 / 32, 1 / 64, 1 / 128, 1 / 256, 1 / 512]
+          }
+        ]
+      }
+    }, {
+      gridOption: {
+        w: 2,
+        h: 3,
+        i: 34,
+      },
+      echartOption: {
+         grid: {
+          left: '0%',
+          right: '0%',
+          bottom: '0%',
+          containLabel: true
+        },
+        xAxis: {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [{
+          data: [150, 230, 224, 218, 135, 147, 260],
+          type: 'line'
+        }]
+      }
+    }
+  ]
 }
